52346f1fd8094612cb072f8d9f8352a0da67725d
Slawek84PL <slapyt84@gmail.com>
2025-03-10 11:44:35 +0100

    Plik przechowuje konfigurację dla ścieżki bazwoej plików i dla adresów email

--- file_manager.py
+++ file_manager.py
@@ -1,30 +1,47 @@
 import glob
 import os.path
-from tkinter import filedialog, messagebox
+from tkinter import filedialog, messagebox, simpledialog
 import pandas as pd
 
 
 class FileManager:
     CONFIG_FILE = "config.txt"
+    EMAIL_RECEIVERS = "email_receivers"
+    BASE_PATH_KW = "base_path_kw"
 
     @staticmethod
-    def get_base_path():
+    def get_config():
+        config = {}
         if os.path.exists(FileManager.CONFIG_FILE):
             with open(FileManager.CONFIG_FILE, "r") as file:
-                return file.read().strip()
-        return None
+                for line in file:
+                    key, value = line.strip().split("=")
+                    config[key] = value
+        return config
+
+    @staticmethod
+    def set_config(key, value):
+        config = FileManager.get_config()
+        config[key] = value
+        with open(FileManager.CONFIG_FILE, "w") as file:
+            for k, v in config.items():
+                file.write(f"{k}={v}\n")
 
     @staticmethod
     def set_base_path():
         path = filedialog.askdirectory()
         if path:
-            with open(FileManager.CONFIG_FILE, "w") as file:
-                file.write(path)
+            FileManager.set_config(FileManager.BASE_PATH_KW, path)
             messagebox.showinfo("Sukces", "Ścieżka zapisana")
 
     @staticmethod
-    def get_files():
+    def get_base_path():
+        return FileManager.get_config().get(FileManager.BASE_PATH_KW)
+
+    @staticmethod
+    def get_files_list():
         base_path = FileManager.get_base_path()
+        print(base_path)
         if not base_path or not os.path.exists(base_path):
             messagebox.showerror("Błąd", "Nie ustawiono poprawnej ściezki bazowej")
             return []
@@ -32,6 +49,20 @@ class FileManager:
         files = glob.glob(os.path.join(base_path, "*"))
         files.sort(key=os.path.getmtime, reverse=True)
         return files
+
+    @staticmethod
+    def get_email_receiver():
+        emails = FileManager.get_config().get(FileManager.EMAIL_RECEIVERS)
+        return emails
+
+    @staticmethod
+    def set_email_receiver():
+        emails = simpledialog.askstring("Odbiorcy e-mail",
+                                        "Podaj adresy e-mail oddzielone przecinkiem:",
+                                        initialvalue=FileManager.get_email_receiver() or "")
+        if emails:
+            FileManager.set_config(FileManager.EMAIL_RECEIVERS, emails)
+            messagebox.showinfo("Sukces", "Adresy zapisane")
 
     @staticmethod
     def load_excel(file_path):
