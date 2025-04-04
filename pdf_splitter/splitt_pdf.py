a35f0be5504071447220e6dddb11c24e10274ece
Slawek84PL <slapyt84@gmail.com>
2025-04-04 21:24:35 +0200

    Naprawiłem statystyki i dodałem zliczanie oszczędzonych naklejek

--- pdf_splitter/splitt_pdf.py
+++ pdf_splitter/splitt_pdf.py
@@ -11,8 +11,9 @@ class PDFGenerator:
     def generate_splits(original_pdf_path, row_data, output_folder):
         try:
             doc = fitz.open(original_pdf_path)
-            files_saved = 0
-            pages_saved = 0
+            files_saved = 1
+            pages_saved = len(doc)
+            stickers_saved = 0
 
             for barcode, pages_str in row_data:
                 if not barcode or not pages_str:
@@ -29,16 +30,16 @@ class PDFGenerator:
 
                 out_path = os.path.join(output_folder, f"{barcode}.pdf")
                 new_doc.save(out_path)
+                stickers_saved += 1
 
-                files_saved += 1
-                pages_saved += len(pages)
+            config = FileManager.get_config()
+            prev_files = int(config.get("split_files_count", 0))
+            prev_pages = int(config.get("split_pages_count", 0))
+            prev_stickers = int(config.get("stickers_saved", 0))
 
-                config = FileManager.get_config()
-                prev_files = int(config.get("split_files_count", 0))
-                prev_pages = int(config.get("split_pages_count", 0))
-
-                FileManager.set_config("split_files_count", str(prev_files + files_saved))
-                FileManager.set_config("split_pages_count", str(prev_pages + pages_saved))
+            FileManager.set_config("split_files_count", str(prev_files + files_saved))
+            FileManager.set_config("split_pages_count", str(prev_pages + pages_saved))
+            FileManager.set_config("saved_stickers", str(prev_stickers + stickers_saved))
 
             return True, None
         except Exception as e:
